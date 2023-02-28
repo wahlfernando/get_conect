4332068c0b7dee172854ac86088acc522637364a
Fernando Alexandre Wahl <wahlfernando@yahoo.com.br>
2023-02-28 11:06:24 -0300

    Initial GetConect

---
+++ android/app/src/main/kotlin/br/comfaw/get_conect_exemple/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.comfaw.get_conect_exemple
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_conect_exemple/pages/home/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      getPages: [
+        GetPage(name: '/', page: () => const HomePage(),)
+      ],
+    );
+  }
+}
+
